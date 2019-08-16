0af507dc281a4afa57ca566721be26e503863e46
Dustin Zubke <dzuble@outlook.com>
2019-08-16 15:25:17 -0600

    added a basic unittest module

---
+++ unittest_sum/.vscode/unittest_sum/__init__.py
@@ -0,0 +1,7 @@
+
+
+def sum(arg):
+    total = 0
+    for val in arg:
+        total += val
+    return total
---
+++ unittest_sum/tests/test.py
@@ -0,0 +1,29 @@
+import unittest
+from fractions import Fraction
+import sys
+sys.path.insert(1, '/Users/dustin/CS/projects/small_projects/unittest_sum/unittest_sum')
+
+
+from my_sum import sum
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+
+    def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+
+        
+if __name__ == '__main__':
+    unittest.main()
